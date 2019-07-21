1b04faa03d78cfe6154020ff49f7c174f2dd5b00
Tom Gregory <t.k.gregory@gmail.com>
2019-07-21 09:57:11 +0100

    Add configurable property to REST response.

--- src/main/java/com/tom/controller/MyController.java
+++ src/main/java/com/tom/controller/MyController.java
@@ -1,13 +1,14 @@
 package com.tom.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
     @GetMapping(value = "/doit")
-    public String getSomething() {
-        return "Something";
+    public String getSomething(@Value("${favouriteFood:cabbage}") String favouriteFood) {
+        return "Your favourite food is " + favouriteFood;
     }
 
 
